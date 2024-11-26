4d0157d4071379698b52a4444addee331a180acf
Muhammadali0210 <theprogramer0210@gmail.com>
2024-11-26 16:50:55 +0500

    get profile info from login

--- src/router/auth.js
+++ src/router/auth.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { checkSchema, validationResult, matchedData } from "express-validator";
 import { loginValidation } from "../utils/validation.js";
 import { User } from "../mongoose/schemas/user.js";
+import { Group } from "../mongoose/schemas/group.js";
 import { comparePassword } from "../utils/hashPassword.js";
 import jwt from 'jsonwebtoken';
 
@@ -16,12 +17,15 @@ router.post('/auth/login', checkSchema(loginValidation), async (req, res) => {
         }
         const data = matchedData(req);
         const user = await User.findOne({login: data.login});
+
+        const userGroup = await Group.findOne({ _id: user.group_ids[0] });
+        const groupName = userGroup.name
         
         if (!user) return res.status(404).send('User not found');
         if (!await comparePassword(data.password, user.password)) return res.status(401).send('Wrong password');
         const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET);
 
-        res.send({token: token, role: user.role, first_name: user.first_name, last_name: user.last_name});
+        res.send({token: token, role: user.role, data: { first_name: user.first_name, last_name: user.last_name, login: user.login, phone: user.phone, telegram_id: user.telegram_id, group_name: groupName || null } });
     } catch (error) {
         res.send(error);
     }
