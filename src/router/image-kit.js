d1db38c24b9530e726ca7f91fbf519ec5afdf8cd
Muhammadali0210 <theprogramer0210@gmail.com>
2024-12-06 11:15:50 +0500

    verification added

--- src/router/image-kit.js
+++ src/router/image-kit.js
@@ -1,4 +1,7 @@
 import ImageKit from "imagekit";
+import { Router } from "express";
+import { verifyAdminOrTeacher } from "../utils/verifyAdminOrTeacher.js";
+const router = Router()
 
 const imagekit = new ImageKit({
   publicKey: "public_xyn9SKy/R0uODN6qsmni/yvenv4=", // O'z publicKey'ingizni kiriting
@@ -6,8 +9,29 @@ const imagekit = new ImageKit({
   urlEndpoint: "https://ik.imagekit.io/njtthrpue", // URL endpoint
 });
 
-app.get("/imagekit-auth", (req, res) => {
-  const authenticationParameters = imagekit.getAuthenticationParameters();
+router.get("/imagekit-auth", verifyAdminOrTeacher, async (req, res) => {
+  const authenticationParameters = await imagekit.getAuthenticationParameters();
   res.json(authenticationParameters);
 });
 
+
+
+router.post("/upload", verifyAdminOrTeacher, async (req, res) => {
+    const { file, fileName } = req.body; // Frontend'dan fayl ma'lumotlarini olish
+  
+    await imagekit
+      .upload({
+        file, // Fayl ma'lumotlari (Base64, URL yoki fayl obyekti)
+        fileName, // Fayl nomi
+        folder: "/uploads", // Ixtiyoriy: Faylni qaysi papkaga yuklash
+      })
+      .then((response) => {
+        res.json(response);
+      })
+      .catch((error) => {
+        console.error("Rasm yuklashda xato:", error);
+        res.status(500).json({ error: "Rasm yuklashda xato yuz berdi." });
+      });
+});
+
+export default router
