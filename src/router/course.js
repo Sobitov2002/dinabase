d5e2ca1be675f13887dd47f143c32bfac21a0d30
Muhammadali0210 <theprogramer0210@gmail.com>
2024-12-16 21:49:40 +0500

    course update and delet

--- src/router/course.js
+++ src/router/course.js
@@ -31,4 +31,22 @@ router.post('/course/create', verifyAdminOrTeacher,  async (req, res) => {
     }
 })
 
+router.put('/course/:id', verifyAdminOrTeacher, async (req, res) => {
+    try {
+        const course = await Course.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(200).send({message: "Kurs muvaffaqqiyatli o'zgartirildi"});
+    } catch (error) {
+        res.status(500).send({message: "Kursni o'zgartirishda hatolik yuz berdi"});
+    }
+})
+
+router.delete('/course/:id', verifyAdminOrTeacher, async (req, res) => {
+    try {
+        const course = await Course.findByIdAndDelete(req.params.id);
+        res.send("Data deleted successfully");
+    } catch (error) {
+        res.send(error);
+    }
+})
+
 export default router;
